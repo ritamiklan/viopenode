96bd27c38b50266c3e69967c3a1a3f9e2c52a9c4
ritamiklan <51336470+ritamiklan@users.noreply.github.com>
2022-05-02 14:35:50 +0300

    next chapter

--- helloexpress/index.js
+++ helloexpress/index.js
@@ -8,6 +8,24 @@ app.get("/", (req, res) => {
   res.send("Hello Express!");
 });
 
+app.get("/home", (req, res) => {
+  res.send("Welcome to our page");
+});
+
+app.get("/home/:name/:age", (req, res) => {
+  if (req.params.age > 18)
+    res.send("Welcome " + req.params.name + " you are " + req.params.age);
+  else res.send("welcome " + req.params.name + " you are too young.");
+});
+
+// app.get("/home/user/", (req, res) => {
+//   res.json({ username: "John" });
+// });
+
+// app.get("/about", (req, res) => {
+//   res.send("About us...");
+// });
+
 app.listen(port, () => {
   console.log(`Server is running on port ${port}.`);
 });
